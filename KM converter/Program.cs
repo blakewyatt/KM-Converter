7bf406ef5355f0ec24bd168de3efff001753dcc0
Blake Wyatt <blakewyatt7@gmail.com>
2021-04-12 12:17:21 -0500

    first commit

---
+++ KM converter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace KM_converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Plese enter a distnace in km: ");
+            ConvertKMtoM(double.Parse(Console.ReadLine()));
+
+        }//End of Main
+
+        //Creating function
+
+        static void ConvertKMtoM(double km)
+        {
+            double miles = km * .6214;
+            Console.WriteLine("{0} km = {1:F2} miles", km, miles);
+        }
+
+        //End of Functions
+    }
+
+
+}
